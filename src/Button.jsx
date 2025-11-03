fe1adf50fbec2f98a4a501d8a243e61848965e9e
Mohammad Ali <aliuser1129@gmail.com>
2025-11-04 00:03:31 +0530

    Added a new paragraph

--- src/Button.jsx
+++ src/Button.jsx
@@ -17,6 +17,10 @@ function Button(){
             <p onMouseOver = {bye}>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos quia distinctio dignissimos nisi eius officiis laboriosam, recusandae enim cumque neque ratione sint numquam deleniti vel, illum, qui facere rem veniam?
             </p>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora magni, esse qui autem doloribus optio repellat harum deserunt. Ipsam error quos repellendus quia sint quas neque nihil nesciunt hic cumque magnam excepturi facere fugit maiores, animi provident labore quidem pariatur dolore dolores reiciendis. Doloremque rem aliquid officiis tempore pariatur placeat autem, harum libero velit quod repudiandae expedita sunt labore ratione maxime perferendis dolore accusantium id minus non beatae modi nesciunt incidunt. Reiciendis assumenda cupiditate adipisci a voluptas exercitationem dolores nesciunt sequi, quas molestiae eligendi laudantium provident libero dignissimos voluptatum voluptatibus quis repellendus ex incidunt excepturi harum quos itaque consectetur! Voluptatum?
+            </p>
             <button onDoubleClick={handleDblClick}>
                 Double click me!
             </button>
